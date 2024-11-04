57dabcb26a43790ee79dc2af77cc7d8e84da48a9
charitraa <stharabi9862187405@gmail.com>
2024-11-04 17:27:36 +0545

    arrange the image

--- Assets/js/partners.js
+++ Assets/js/partners.js
@@ -3,7 +3,7 @@
 const gallery = document.getElementById("gallery");
 
 // Array of image URLs
-const images = ['image/partner/1.png', 'image/partner/Aakash Blowers.png', 'image/partner/Axis.jpg', 'image/partner/Global absorbant pvt ltd.png', 'image/partner/Initiative Water.png', 'image/partner/Mann+Hummel.png', 'image/partner/noble-chlor.jpg', 'image/partner/Pingxiang Tiansheng Chemical Equipment Co. Ltd..png', 'image/partner/Pure-N-safe.png', 'image/partner/Seko.png', 'image/partner/Watch-Water.png', 'image/partner/Watco Odissa.png']
+const images = ['image/partner/Mann+Hummel.png', 'image/partner/Seko.png', 'image/partner/Axis.jpg', 'image/partner/Global absorbant pvt ltd.png', 'image/partner/Initiative Water.png', 'image/partner/noble-chlor.jpg', 'image/partner/Pingxiang Tiansheng Chemical Equipment Co. Ltd..png', 'image/partner/Pure-N-safe.png', 'image/partner/Watch-Water.png', 'image/partner/Watco Odissa.png', 'image/partner/Aakash Blowers.png',]
 
 // Add images dynamically to the gallery
 images.forEach(image => {
