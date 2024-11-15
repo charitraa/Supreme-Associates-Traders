8153cd8f5f7f486b47758db1363b22762a5feb86
charitraa <stharabi9862187405@gmail.com>
2024-11-15 11:01:12 +0545

    make partner page responsive

--- Assets/js/partners.js
+++ Assets/js/partners.js
@@ -3,7 +3,7 @@
 const gallery = document.getElementById("gallery");
 
 // Array of image URLs
-const images = ['image/partner/Mann+Hummel.png', 'image/partner/Seko.png', 'image/partner/Axis.jpg', 'image/partner/Global absorbant pvt ltd.png', 'image/partner/Initiative Water.png', 'image/partner/noble-chlor.jpg', 'image/partner/Pingxiang Tiansheng Chemical Equipment Co. Ltd..png', 'image/partner/Pure-N-safe.png', 'image/partner/Watch-Water.png', 'image/partner/Watco Odissa.png', 'image/partner/Aakash Blowers.png',]
+const images = ['image/partner/Mann+Hummel.png', 'image/partner/Seko.png', 'image/partner/7.png', 'image/partner/1.png', 'image/partner/Initiative Water.png', 'image/partner/noble-chlor.jpg', 'image/partner/Pingxiang Tiansheng Chemical Equipment Co. Ltd..png', 'image/partner/Pure-N-safe.png', 'image/partner/Watch Water.png', 'image/partner/Watco.png', 'image/partner/Akash.png',]
 
 // Add images dynamically to the gallery
 images.forEach(image => {
